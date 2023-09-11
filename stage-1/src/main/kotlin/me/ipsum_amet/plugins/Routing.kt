7b8d355bdde5b7bb2d6a5efebd4a0845236e7324
skeizable <kibet.koskei@proton.me>
2023-09-11 13:12:24 +0300

    implement the stage1 requirement

--- stage-1/src/main/kotlin/me/ipsum_amet/plugins/Routing.kt
+++ stage-1/src/main/kotlin/me/ipsum_amet/plugins/Routing.kt
@@ -1,13 +1,61 @@
 package me.ipsum_amet.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Add this `/api?slack_name=<Name>&track=<TrackName>` to the base URL to get what you came for :).")
+        }
+        get("/api") {
+
+            try {
+                val queryParameters = call.request.queryParameters
+
+                val slackName = queryParameters["slack_name"]
+
+                val track = queryParameters["track"]
+
+                val currentDateTime = Clock.System.now()
+
+                val currentDayOfWeek = currentDateTime.toLocalDateTime(TimeZone.UTC).dayOfWeek
+
+                val utcTime = currentDateTime.toString()
+
+
+                val response = Stage1Response(
+                    slackName = slackName,
+                    currentDay = currentDayOfWeek.toString(),
+                    track = track,
+                    utcTime = utcTime,
+                    githubFileUrl = "TODO",
+                    githubRepoUrl = "TODO",
+                    statusCode = HttpStatusCode.OK.value
+                )
+
+                call.respond(HttpStatusCode.OK, response)
+
+            } catch (ex: Exception) {
+                call.respond(ex.localizedMessage)
+            }
         }
     }
 }
+
+@Serializable
+data class Stage1Response(
+    val slackName: String?,
+    val currentDay: String?,
+    val utcTime: String?,
+    val track: String?,
+    val githubFileUrl: String?,
+    val githubRepoUrl: String?,
+    val statusCode: Int
+)
